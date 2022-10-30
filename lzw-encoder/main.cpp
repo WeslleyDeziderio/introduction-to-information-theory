b36a67ec0d8c0942e76929728e8e175b7489a221
Weslley Dezidério <weslleydeziderio@gmail.com>
2022-10-30 20:05:28 -0300

    add typecode

--- lzw-encoder/main.cpp
+++ lzw-encoder/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
+#include "./src/encoder.cpp"
+#include "./src/decoder.cpp"
+
 int main(int argc, char *argv[]) {
 
     if(argc != 1) {
         std::cout << "Erro: número de argumentos inválidos!" << std::endl;
     }
+
+    Encoder enc;
+    enc.printHello();
 
     return 0;
 }
--- lzw-encoder/src/encoder.cpp
+++ lzw-encoder/src/encoder.cpp
@@ -1,2 +1,11 @@
 #include "../libs/encoder.hpp"
 
+void Encoder::printHello() {
+    std::cout << "Hello world!" << std::endl;
+}
+
+Encoder::Encoder() {
+    typedef 
+    using tipoCodigo = uint16_t; // tipo de dado utilizado para armazenar os dados
+
+}
